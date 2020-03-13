464f500e0bf41b4c532f7fdd7fb511c1e4396306
khanhdrag9 <tkhanh783@gmail.com>
2020-03-13 22:04:24 +0700

    Added Weapon system

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    [System.Serializable]
+    public class Weapon
+    {
+        public List<InputAction> oneHand;
+        public List<InputAction> twoHand;
+    }
+
+    public class Inventory : MonoBehaviour
+    {
+        public Weapon curWeapon;
+        public void Init()
+        {
+
+        }
+    }
+}
